6c126f1cf61f6cc4516619c385aa05a1e7c52f83
Deven <devenpuri29@gmail.com>
2026-01-22 01:36:26 +0530

    Fix: export Express app for Vercel serverless

--- server.js
+++ server.js
@@ -18,7 +18,4 @@ app.get("/", (req, res) => {
   res.send("REST API is running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
